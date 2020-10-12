d4768de634168178f065706b354c4a2d7605d2d2
Dênis Schimidt <schimidt@pop-os.localdomain>
2020-10-12 17:32:32 -0300

    Cria estrutura inicial do projeto com maven

---
+++ src/main/java/com/viavarejo/vendas/parcelamento/CalculadoraParcelasApplication.java
@@ -0,0 +1,12 @@
+package com.viavarejo.vendas.parcelamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraParcelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraParcelasApplication.class, args);
+	}
+}
